d4fc23a29d20ae36432e0ad1b6598d36c5b14790
Rasesh Patel <raspat1@gmail.com>
2018-07-25 22:51:13 -0400

    Rewrite in ruby
    I saw this old repo lol. It's my first one on github. For nostalgia
    wondered how long this would take 5 years later. Luckily,just a few
    moments. BUT. A bit of code is never done. I wonder how performant
    I can make this. Ruby instead of Java but smarter algs can give
    this a run for its money I'm sure.

---
+++ adult_ras/permutable_prime.rb
@@ -0,0 +1,77 @@
+require 'prime'
+
+class PermutablePrime
+  attr_accessor :permuter
+
+  def self.build
+    new(Permuter.new)
+  end
+
+  def initialize(permuter)
+    @permuter = permuter
+  end
+
+  def call(max)
+    all_pps = []
+
+    Prime.each(max) do |prime|
+      # skip if there is an even number a 0 or a 5
+      # those numbers will be at the end in some permutation
+      # if they are at the end the nuber cannot be prime (except for the trivial cases)
+      next if has_bad_digit(prime) # 20x perf lol
+
+      # Get all permuations
+      permutations = permuter.call(prime)
+
+      # Check primality
+      next if permutations.any? { |permutation| !Prime.prime?(permutation.to_i) }
+
+      puts prime
+      all_pps << prime
+    end
+
+    all_pps
+  end
+
+  private
+
+  def has_bad_digit(prime)
+    prime > 10 && !prime.to_s.match(/0|5|2|4|6|8/).nil?
+  end
+end
+
+# Perf improvement
+# check primality as permutations are being generated
+# Do a DFS with checks at the bottom nodes rather than a BFS generation
+# with checks at last layer or after last layer
+
+# Generate all numbers created by permuting the digits of a given number
+class Permuter
+  def initialize
+  end
+
+  def call(number)
+    perms = ['']
+    chars = number.to_s.split('')
+
+    chars.size.times do |iteration|
+      new_perms = []
+
+      perms.each do |perm|
+        chars.each do |char|
+          next if perm.count(char) == chars.count(char)
+          new_perms << perm + char
+        end
+      end
+
+      perms = new_perms
+    end
+
+    perms
+  end
+end
+
+puts Time.now
+all_pps = PermutablePrime.build.call(10**6)
+puts all_pps.join(', ')
+puts Time.now
